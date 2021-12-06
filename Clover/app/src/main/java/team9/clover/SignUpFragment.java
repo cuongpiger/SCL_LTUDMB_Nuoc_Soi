ddd6c129aa1a7d893e3983ac1f4f495673d5599f
Manh Cuong <cuongpigerr@gmail.com>
2021-12-07 01:05:18 +0700

    done registration for new user

--- Clover/app/src/main/java/team9/clover/SignUpFragment.java
+++ Clover/app/src/main/java/team9/clover/SignUpFragment.java
@@ -1,28 +1,55 @@
 package team9.clover;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.apache.commons.text.WordUtils;
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 import team9.clover.Module.Reusable;
 
 
 public class SignUpFragment extends Fragment {
 
-    TextInputLayout tilFullName, tilEmail, tilPassword, tiiRetype;
+    TextInputLayout tilFullName, tilEmail, tilPassword, tilRetype;
+    MaterialButton mbtSignUp;
     MaterialTextView mtvSignIn;
     FrameLayout floLogin;
+    ProgressBar pgbSignUp;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     public SignUpFragment() { }
 
@@ -30,12 +57,15 @@ public class SignUpFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFirebase();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         referWidgets(view);
+
+
         return view;
     }
 
@@ -47,7 +77,12 @@ public class SignUpFragment extends Fragment {
 
     private void referWidgets(View view) {
         tilEmail = view.findViewById(R.id.tilEmail);
+        tilFullName = view.findViewById(R.id.tilFullName);
+        tilPassword = view.findViewById(R.id.tilPassword);
+        tilRetype = view.findViewById(R.id.tilRetype);
         mtvSignIn = view.findViewById(R.id.mtvSignIn);
+        mbtSignUp = view.findViewById(R.id.mbtSignUp);
+        pgbSignUp = view.findViewById(R.id.pgbSignUp);
         floLogin = getActivity().findViewById(R.id.floLogin);
     }
 
@@ -58,6 +93,120 @@ public class SignUpFragment extends Fragment {
                 setFragment(new SignInFragment());
             }
         });
+
+        tilEmail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String email = tilEmail.getEditText().getText().toString();
+                tilEmail.setErrorEnabled(true);
+                if (email.isEmpty()) {
+                    tilEmail.setError(getString(R.string.email_empty));
+                } else if (!EmailValidator.getInstance().isValid(email)) {
+                    tilEmail.setError(getString(R.string.email_invalid));
+                } else {
+                    tilEmail.setErrorEnabled(false);
+                    tilEmail.setError("");
+                }
+            }
+        });
+
+        tilFullName.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String fullName = tilFullName.getEditText().getText().toString();
+                tilFullName.setErrorEnabled(true);
+                if (fullName.isEmpty()) {
+                    tilFullName.setError(getString(R.string.name_empty));
+                } else {
+                    tilFullName.setErrorEnabled(false);
+                    tilFullName.setError("");
+                }
+            }
+        });
+
+        tilPassword.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String password = tilPassword.getEditText().getText().toString();
+                tilPassword.setErrorEnabled(true);
+                if (password.isEmpty()) {
+                    tilPassword.setError(getString(R.string.password_empty));
+                } else if (password.length() < 8) {
+                    tilPassword.setError(getString(R.string.password_short));
+                } else if (password.length() > 18) {
+                    tilPassword.setError(getString(R.string.password_long));
+                } else {
+                    tilPassword.setErrorEnabled(false);
+                    tilPassword.setError("");
+                }
+            }
+        });
+
+        tilRetype.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String password = tilPassword.getEditText().getText().toString();
+                String retype = tilRetype.getEditText().getText().toString();
+                tilRetype.setErrorEnabled(true);
+                if (retype.isEmpty()) {
+                    tilRetype.setError(getString(R.string.retype_empty));
+                } else if (!retype.equals(password)) {
+                    tilRetype.setError(getString(R.string.retype_invalid));
+                } else {
+                    tilRetype.setErrorEnabled(false);
+                    tilRetype.setError("");
+                }
+            }
+        });
+
+        mbtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!(tilEmail.isErrorEnabled() && tilFullName.isErrorEnabled() && tilPassword.isErrorEnabled() && tilRetype.isErrorEnabled())) {
+                    signUp();
+                }
+            }
+        });
     }
 
     private void setFragment(Fragment fragment) {
@@ -65,5 +214,64 @@ public class SignUpFragment extends Fragment {
         fragmentTransaction.setCustomAnimations(R.anim.slide_from_left, R.anim.slideout_from_right);
         fragmentTransaction.replace(floLogin.getId(), fragment);
         fragmentTransaction.commit();
+    }
+
+    private void setFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+    }
+
+    private void addNewUser() {
+        firebaseFirestore.collection("User")
+                .add(Collections.singletonMap("fullName", WordUtils.capitalize(tilFullName.getEditText().getText().toString().trim())))
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful()) {
+                            Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+                            getActivity().finish();
+                            startActivity(mainIntent);
+                        } else {
+                            pgbSignUp.setVisibility(View.INVISIBLE);
+                            mbtSignUp.setClickable(true);
+                            new MaterialAlertDialogBuilder(getContext(), R.style.ThemeOverlay_App_MaterialAlertDialog).setTitle(getString(R.string.error_title))
+                                    .setMessage(getString(R.string.sign_up_failure))
+                                    .setCancelable(false)
+                                    .setPositiveButton(getString(R.string.close), new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            dialogInterface.dismiss();
+                                        }
+                                    }).create().show();
+                        }
+                    }
+                });
+    }
+
+    private void signUp() {
+        pgbSignUp.setVisibility(View.VISIBLE);
+        mbtSignUp.setClickable(false);
+        String email = tilEmail.getEditText().getText().toString().trim();
+        String password = tilPassword.getEditText().getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    addNewUser();
+                } else {
+                    pgbSignUp.setVisibility(View.INVISIBLE);
+                    mbtSignUp.setClickable(true);
+                    new MaterialAlertDialogBuilder(getContext(), R.style.ThemeOverlay_App_MaterialAlertDialog).setTitle(getString(R.string.error_title))
+                            .setMessage(getString(R.string.sign_up_failure))
+                            .setCancelable(false)
+                            .setPositiveButton(getString(R.string.close), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            }).create().show();
+                }
+            }
+        });
     }
 }
