212eb6c8002bc09985263cdcc9658e7bd94d5da0
Manh Cuong <cuongpigerr@gmail.com>
2021-12-08 22:53:31 +0700

    modify splash screen effect animation

--- Clover/app/src/main/java/team9/clover/SplashActivity.java
+++ Clover/app/src/main/java/team9/clover/SplashActivity.java
@@ -2,16 +2,12 @@ package team9.clover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -53,15 +49,10 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void setAnimation() {
-        final Animation zoomin = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
-        tvCopyright.animate().alpha(0f).setDuration(0);
-        ivLogo.setAnimation(zoomin);
-
-        zoomin.setAnimationListener(new Animation.AnimationListener() {
+        final Animation splashScreen = AnimationUtils.loadAnimation(this, R.anim.splash_screen);
+        splashScreen.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
+            public void onAnimationStart(Animation animation) { }
 
             @Override
             public void onAnimationEnd(Animation animation) {
@@ -70,10 +61,11 @@ public class SplashActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
+            public void onAnimationRepeat(Animation animation) { }
         });
+
+        tvCopyright.animate().alpha(0f).setDuration(0);
+        ivLogo.startAnimation(splashScreen);
     }
 
     private void runNextActivity() {
