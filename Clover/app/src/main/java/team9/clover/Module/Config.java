c223d3fa4f3f45ffe5973ffa6095b1e2eb0fe4d1
Manh Cuong <cuongpigerr@gmail.com>
2021-12-09 23:06:30 +0700

    done for navigation side

--- Clover/app/src/main/java/team9/clover/Module/Config.java
+++ Clover/app/src/main/java/team9/clover/Module/Config.java
@@ -5,8 +5,8 @@ import java.util.Calendar;
 import team9.clover.R;
 
 public class Config {
-    public static final int TURN_SIGN_IN_DELAY = 0; // 3000
-    public static final int COPYRIGHT_DELAY = 0; // 800
+    public static final int TURN_SIGN_IN_DELAY = 3000; // 3000
+    public static final int COPYRIGHT_DELAY = 800; // 800
     public static final int LOGO_DELAY = 1000; // 1000
     public static final String COPYRIGHT = String.format("Copyright %d - Team9", Calendar.getInstance().get(Calendar.YEAR));
 }
