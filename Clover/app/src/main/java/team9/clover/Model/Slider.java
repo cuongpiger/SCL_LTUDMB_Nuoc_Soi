4c07b47ee80344fa8f51470fbcbdc47539940f1c
Manh Cuong <cuongpigerr@gmail.com>
2021-12-10 22:46:44 +0700

    done slider banner at home page

---
+++ Clover/app/src/main/java/team9/clover/Model/Slider.java
@@ -0,0 +1,17 @@
+package team9.clover.Model;
+
+public class Slider {
+    private int banner;
+
+    public Slider(int banner) {
+        this.banner = banner;
+    }
+
+    public int getBanner() {
+        return banner;
+    }
+
+    public void setBanner(int banner) {
+        this.banner = banner;
+    }
+}
--- Clover/app/src/main/java/team9/clover/ui/home/HomeFragment.java
+++ Clover/app/src/main/java/team9/clover/ui/home/HomeFragment.java
@@ -1,7 +1,9 @@
 package team9.clover.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,12 +15,18 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import team9.clover.Model.Category;
+import team9.clover.Model.Slider;
 import team9.clover.Module.CategoryAdapter;
+import team9.clover.Module.Config;
+import team9.clover.Module.SliderAdapter;
 import team9.clover.R;
 import team9.clover.databinding.FragmentHomeBinding;
 
@@ -26,8 +34,14 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
     RecyclerView categoryRecyclerView;
     CategoryAdapter categoryAdapter;
+
+    ViewPager bannerSliderViewPager;
+    List<Slider> sliderList;
+    Timer timer;
+    int currentPage = 2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,7 +52,8 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         referWidgets(root);
-        setView();
+        setViewCategory();
+        setViewBannerSlider(root);
 
         return root;
     }
@@ -53,7 +68,7 @@ public class HomeFragment extends Fragment {
         categoryRecyclerView = view.findViewById(R.id.rvCategory);
     }
 
-    private void setView() {
+    private void setViewCategory() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         categoryRecyclerView.setLayoutManager(linearLayoutManager);
@@ -70,5 +85,96 @@ public class HomeFragment extends Fragment {
         categoryAdapter = new CategoryAdapter(categoryList);
         categoryRecyclerView.setAdapter(categoryAdapter);
         categoryAdapter.notifyDataSetChanged();
+    }
+
+    private void setViewBannerSlider(View view) {
+        bannerSliderViewPager = view.findViewById(R.id.vpBannerSlider);
+        sliderList = new ArrayList<Slider>();
+        sliderList.add(new Slider(R.drawable.ic_baseline_email_24));
+        sliderList.add(new Slider(R.drawable.ic_baseline_attach_email_24));
+
+        sliderList.add(new Slider(R.drawable.banner1));
+        sliderList.add(new Slider(R.drawable.banner2));
+        sliderList.add(new Slider(R.drawable.banner3));
+        sliderList.add(new Slider(R.drawable.banner4));
+        sliderList.add(new Slider(R.drawable.banner5));
+        sliderList.add(new Slider(R.drawable.ic_baseline_person_24));
+        sliderList.add(new Slider(R.drawable.ic_baseline_email_24));
+        sliderList.add(new Slider(R.drawable.ic_baseline_attach_email_24));
+
+
+        sliderList.add(new Slider(R.drawable.ic_bell_24));
+        sliderList.add(new Slider(R.drawable.ic_cart_24));
+
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+        startBannerSlideShow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                pageLooper();
+                stopBannerSliderShow();
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) startBannerSlideShow();
+                return false;
+            }
+        });
+    }
+
+    private void pageLooper() {
+        if (currentPage == sliderList.size() - 2) {
+            currentPage = 2;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+
+        if (currentPage == 1) {
+            currentPage = sliderList.size() - 3;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+    }
+
+    private void startBannerSlideShow() {
+        Handler handler = new Handler();
+        Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage >= sliderList.size()) currentPage = 1;
+                bannerSliderViewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, Config.BANNER_SLIDER_DELAY, Config.BANNER_SLIDER_PERIOD);
+    }
+
+    private void stopBannerSliderShow() {
+        timer.cancel();
     }
 }
