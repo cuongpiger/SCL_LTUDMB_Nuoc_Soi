b255409c2f70c7e3033e8e6dfe26d4da4674bed7
Manh Cuong <cuongpigerr@gmail.com>
2021-12-19 15:44:29 +0700

    This is Manh Cuong's commit 🌻.

---
+++ Clover/app/src/main/java/team9/clover/MyAccountFragment.java
@@ -0,0 +1,29 @@
+package team9.clover;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MyAccountFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyAccountFragment extends Fragment {
+
+    public MyAccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_account, container, false);
+    }
+}
