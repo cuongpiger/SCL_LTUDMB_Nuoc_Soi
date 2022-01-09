8d9f14b39dd63ee7969e10b4dda07bb8a3a060db
Manh Cuong <cuongpigerr@gmail.com>
2022-01-09 15:23:29 +0700

    This is Manh Cuong's commit 🌻.

---
+++ Demo4Slide/app/src/main/java/team9/demo4slide/LoginActivity.java
@@ -0,0 +1,23 @@
+package team9.demo4slide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
+
+
